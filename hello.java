911384c4ae23860b7e9c491b815a0c8be09608e4
srinikhi29 <srinikhigaddipati@gmail.com>
2025-03-07 09:12:18 +0530

    new changes
    changes

--- hello.java
+++ hello.java
@@ -1,4 +1,5 @@
 public class HelloWorld {
+    // just adding a comment line for execution purpose 
     public static void main(String[] args) {
         // Print Hello, World! to the console
         System.out.println("Hello, World!");
